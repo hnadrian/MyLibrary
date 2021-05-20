f2803f7d89d842e934a4eeb018be28ca55fda4d4
hnadrian <hngt.adrian@gmail.com>
2021-05-19 21:42:40 -0700

    Add skeleton script js file

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = []
+
+function Book(title, author, pageNum, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.isRead = isRead;
+}
+  
+function addBookToLibrary() {
+    
+}
