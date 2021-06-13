02e946af0339afe317dd6558fbe4f72f5a6e3aa1
hnadrian <hngt.adrian@gmail.com>
2021-06-13 13:29:09 -0700

    Remove year field, Add create book card

--- script.js
+++ script.js
@@ -10,26 +10,25 @@ const formSubmitButton = document.querySelector('#submit-button');
 formSubmitButton.addEventListener('click', addNewBook);
 
 const newBookForm = document.querySelector('#new-book-form');
+newBookForm.addEventListener('submit', handleForm);
 const titleInput = document.querySelector('#title-input'),
     authorInput = document.querySelector('#author-input'),
-    yearInput = document.querySelector('#year-input'),
     numPageInput = document.querySelector('#num-page-input');
 
 
 const newBookFormModal = document.querySelector('.modal');
 const newBookFormModalBG = document.querySelector('.bg-modal');
 
+const bookshelf = document.querySelector('#bookshelf-container');
+
+
 const UNKOWN_VARIABLE = 'Unknown';
 
-function Book(title = UNKOWN_VARIABLE, author = UNKOWN_VARIABLE, pageNum = 0, isRead = false) {
+function Book(title, author, pageNum, isRead = false) {
     this.title = title;
-    this.author = author;
-    this.pageNum = pageNum;
+    this.author = (author === '') ? UNKOWN_VARIABLE : author;
+    this.pageNum = (pageNum === '') ? 0 : pageNum;
     this.isRead = isRead;
-}
-
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
 }
 
 function addNewBook(e) {
@@ -37,17 +36,28 @@ function addNewBook(e) {
         let newBook = new Book(
             document.querySelector('#title-input').value,
             document.querySelector('#author-input').value,
-            document.querySelector('#year-input').value,
             document.querySelector('#num-page-input').value,
             document.querySelector('#read-button').checked
         );
         myLibrary.push(newBook);
+        addBookToDisplay(newBook);
     }
+    hideForm();
     console.log(myLibrary);
 }
 
-function displayBooksInLibrary() {
-    console.log();
+function addBookToDisplay(newBook) {
+    let newBookDiv = document.createElement('div');
+    newBookDiv.className = 'book';
+    let newBookTitle = document.createElement('h4');
+    newBookDiv.appendChild(newBookTitle);
+    newBookTitle.innerText = newBook.title;
+    let newBookReadStatus = (newBook.isRead) ? 'Finished' : 'Unfinished';
+    newBookDiv.innerHTML += 'by ' + newBook.author.italics() +  '<br />'
+        + 'Total pages: ' + newBook.pageNum + ' pgs' +  '<br />'
+        + 'Read status: ' + newBookReadStatus;
+    
+    bookshelf.appendChild(newBookDiv);
 }
 
 function showForm(e) {
@@ -57,13 +67,22 @@ function showForm(e) {
 
 function hideForm(e) {
     newBookFormModalBG.style.display = 'none';
+    newBookForm.reset(); //Error not focusable
+}
+
+//Prevents submit button from refreshing
+function handleForm(e) {
+    e.preventDefault();
 }
 
 function main() {
     let testBook1 = new Book('Harry Porter', 'Lady Bird',122, false);
     let testBook2 = new Book('The Greatest Book Of All', 'John Doe',6622, true);
-    addBookToLibrary(testBook1);
-    addBookToLibrary(testBook2);
+    myLibrary.push(testBook1);
+    myLibrary.push(testBook2);
+    addBookToDisplay(testBook1);
+    addBookToDisplay(testBook2);
+
 }
 
 main()
